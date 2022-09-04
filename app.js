55d5a224d0b0e25fe2afed287c0568598e6afaf8
Pratik Mazumdar <45979935+LUCIF680@users.noreply.github.com>
2022-09-04 06:38:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const Handlebars = require("handlebars");
 require('dotenv').config()
 const chokidar = require('chokidar');
+const childProcess = require('child_process');
+
 
 (async () => {  
     try {
-        await fs.promises.access(process.env.BUILD_FOLDER_PATH);
+        await fs.access(process.env.BUILD_FOLDER_PATH);
     }catch(err) { 
-        await fs.promises.mkdir(process.env.BUILD_FOLDER_PATH);
+        await fs.mkdir(process.env.BUILD_FOLDER_PATH);
     }   
 
     const directories = ['views', 'viewModels', 'models']
     await directories.forEach(async (directory) => {
         try {
-            await fs.promises.access(`${process.env.BUILD_FOLDER_PATH}/${directory}`);
+            await fs.access(`${process.env.BUILD_FOLDER_PATH}/${directory}`);
         }catch(err) { 
-            await fs.promises.mkdir(`${process.env.BUILD_FOLDER_PATH}/${directory}`);
+            await fs.mkdir(`${process.env.BUILD_FOLDER_PATH}/${directory}`);
         }   
     })
 })()
 
 chokidar.watch('src/').on('add', async (srcPath) => {
     const buildPath = srcPath.replace('src',process.env.BUILD_FOLDER_PATH);
-    const data = await fs.promises.readFile(srcPath,'utf8')
+    const data = await fs.readFile(srcPath,'utf8');
     const template = Handlebars.compile(data)
-    fs.promises.writeFile(
+    fs.writeFile(
         buildPath,
         template({
             "staticUrl" : process.env.STATICURL,
             "baseUrl" : process.env.BASEURL
         })
         ,{ flag: 'w+' }
-    )
-});
-
-    
+    ) 
+    console.log(`Output is '${buildPath}'`);
+}); 
+childProcess.exec(`npx tailwindcss -i src/views/style.css -o ${process.env.BUILD_FOLDER_PATH}/views/style.css --watch`);
+console.log(`Output of css file is '${process.env.BUILD_FOLDER_PATH}/views/style.css'`);
