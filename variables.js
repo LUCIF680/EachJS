6787f8eff79f489ee5b74f3174ff61536b3f432f
Pratik <pratikmazumdar@quantiply.tech~>
2022-09-04 07:01:54 -0400

    Added hello world

--- variables.js
+++ variables.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 module.exports ={
     "staticUrl" : process.env.STATICURL,
-    "baseUrl" : process.env.BASEURL
+    "baseUrl" : process.env.BASEURL,
+    "title": "Hello World"
 }
